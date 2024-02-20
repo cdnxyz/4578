535feb0699c4927bdf16406195f3a5e04d20b9fa
cdnxyz <151710412+cdnxyz@users.noreply.github.com>
2024-02-20 19:28:56 +0600

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -12,29 +12,29 @@ class Var(object):
     API_ID = int(getenv('API_ID', '19061625'))
     API_HASH = str(getenv('API_HASH', '4534b1d0579daa0f770f4f3d520c94d5'))
     BOT_TOKEN = str(getenv('BOT_TOKEN', '6914582967:AAGIeiaVDRaLRa_CLfFFqJAo5O2xNPHJs6U'))
-    name = str(getenv('name', 'shkaib_file_upload_bot'))
+    name = str(getenv('name', 'File BOT'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-2070434580'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001826781136'))
     PORT = int(getenv('PORT', '8080'))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', ''))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '45.83.122.156'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6794257273").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'expertskb'))
     if 'DYNO' in environ:
-        ON_HEROKU = True
+        ON_HEROKU = False
         APP_NAME = str(getenv('APP_NAME'))
     
     else:
         ON_HEROKU = False
-    FQDN = 'lnk2.eu.org' # str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
-    HAS_SSL=bool(getenv('HAS_SSL',True))
+    FQDN = str(getenv('FQDN', 'lnk2.eu.org')) if not ON_HEROKU or getenv('FQDN', 'lnk2.eu.org') else APP_NAME+'.herokuapp.com'
+    HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://wearesambd:GqEMeHZ1mSqMFABS@movie.vc12ke4.mongodb.net/telegram?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', ''))
-    BANNED_CHANNELS = list(set(int(x) for x in getenv("BANNED_CHANNELS", "").split(",") if x.strip()))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'shkaib_file_upload_bot'))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split())) 
